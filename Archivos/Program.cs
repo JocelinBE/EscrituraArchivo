b6ca08d3d595fe600b0b1b6d3c08acfe1fb25921
JocelinBE <l21211919@tectijuana.edu.mx>
2022-06-09 12:26:15 -0700

    Escritura Archivo

---
+++ Archivos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;   //  Se hace uso de la librería System.IO
+
+namespace Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //CREAR ARCHIVO DE TEXTO EN BIN/DEBUG
+
+                //Leer archivo
+                StreamReader sr = new StreamReader("Jocelin.txt");
+                string línea;
+
+                //Recorrer el archivo
+                while ((línea = sr.ReadLine()) != null) // Revisar si el archivo tiene texto
+                {
+
+                    Console.WriteLine(línea); //Se escribe la línea en consola
+                }
+                //Cerrar el archivo
+                sr.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("No se ha encontrado el archivo.");
+            }
+            Console.ReadLine();
+          
+            
+        }
+        
+            
+           
+    }
+}
